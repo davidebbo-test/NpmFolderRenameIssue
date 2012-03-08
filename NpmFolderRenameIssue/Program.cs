b0057447a6c2242d5883c651bba3d44d3cfcf5f2
davidebbo <david.ebbo@microsoft.com>
2012-03-07 23:40:59 -0800

    Cleanup

--- NpmFolderRenameIssue/Program.cs
+++ NpmFolderRenameIssue/Program.cs
@@ -16,16 +16,15 @@ namespace NpmFolderRenameIssue
             }
 
             // This file should not exist yet
+            // Note that the presence of this existence check is what triggers the bug below!!
             Console.WriteLine("Exists (should be false): " + File.Exists("test/test2/file20"));
 
             // Create a directory with 20 files
-            Directory.CreateDirectory("test");
-            Directory.CreateDirectory("test/subdir");
             Directory.CreateDirectory("test/subdir/test");
 
             for (var i = 1; i <= 20; i++)
             {
-                File.WriteAllBytes("test/subdir/test/file" + i, buf);
+                File.WriteAllBytes(String.Format("test/subdir/test/file{0:00}", i), buf);
             }
 
             // Rename the directory
@@ -33,7 +32,7 @@ namespace NpmFolderRenameIssue
 
             var start = DateTime.UtcNow;
 
-            // Read the directory
+            // List the files. Here, they all show up
             foreach (var path in Directory.GetFiles("test/test2"))
             {
                 Console.WriteLine(path);
@@ -41,6 +40,7 @@ namespace NpmFolderRenameIssue
 
             for (; ; )
             {
+                // This should be true, but when running on a (cross machine) UNC share, it takes almost 5 seconds to become true!
                 if (File.Exists("test/test2/file20")) break;
 
                 Console.WriteLine("After {0} milliseconds, test/test2/file20 doesn't show as existing", (DateTime.UtcNow - start).TotalMilliseconds);
