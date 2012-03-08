17ca4f6ea485ee6ca38380bb383837fb8aa497cf
davidebbo <david.ebbo@microsoft.com>
2012-03-07 23:55:14 -0800

    More simplifications

--- NpmFolderRenameIssue/Program.cs
+++ NpmFolderRenameIssue/Program.cs
@@ -8,31 +8,20 @@ namespace NpmFolderRenameIssue
     {
         static void Main(string[] args)
         {
-            // Create a buffer of some size
-            var buf = new byte[1024];
-            for (int n = 0; n < buf.Length; n++)
-            {
-                buf[n] = 66;
-            }
-
-            // This file should not exist yet
+            // This file doesn't exist yet
             // Note that the presence of this existence check is what triggers the bug below!!
-            Console.WriteLine("Exists (should be false): " + File.Exists("test/test2/file20"));
+            Console.WriteLine("Exists (should be false): " + File.Exists("test/test2/myfile"));
 
-            // Create a directory with 20 files
+            // Create a directory, with a file in it
             Directory.CreateDirectory("test/subdir/test");
-
-            for (var i = 1; i <= 20; i++)
-            {
-                File.WriteAllBytes(String.Format("test/subdir/test/file{0:00}", i), buf);
-            }
+            File.WriteAllText("test/subdir/test/myfile", "Hello");
 
             // Rename the directory
             Directory.Move("test/subdir/test", "test/test2");
 
             var start = DateTime.UtcNow;
 
-            // List the files. Here, they all show up
+            // List the files at the new location. Here, our file shows up fine
             foreach (var path in Directory.GetFiles("test/test2"))
             {
                 Console.WriteLine(path);
@@ -40,14 +29,15 @@ namespace NpmFolderRenameIssue
 
             for (; ; )
             {
-                // This should be true, but when running on a (cross machine) UNC share, it takes almost 5 seconds to become true!
-                if (File.Exists("test/test2/file20")) break;
+                // Now do a simple existence test. It should also be true, but when running on a (cross machine) UNC share,
+                // it takes almost 5 seconds to become true!
+                if (File.Exists("test/test2/myfile")) break;
 
-                Console.WriteLine("After {0} milliseconds, test/test2/file20 doesn't show as existing", (DateTime.UtcNow - start).TotalMilliseconds);
+                Console.WriteLine("After {0} milliseconds, test/test2/myfile doesn't show as existing", (DateTime.UtcNow - start).TotalMilliseconds);
                 Thread.Sleep(100);
             }
 
-            Console.WriteLine("After {0} milliseconds, test/test2/file20 correctly shows as existing!", (DateTime.UtcNow - start).TotalMilliseconds);
+            Console.WriteLine("After {0} milliseconds, test/test2/myfile correctly shows as existing!", (DateTime.UtcNow - start).TotalMilliseconds);
             Console.ReadLine();
         }
     }
